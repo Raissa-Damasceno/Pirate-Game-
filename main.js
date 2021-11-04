399c647e0850a580ca7b23f7d9a806c30ab15874
Raissa Damasceno <raissa.cod@gmail.com>
2021-11-04 21:43:57 +0000

    Game

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
+
+
 let canvas = document.querySelector("#my-canvas");
 let ctx = canvas.getContext("2d");
 
 let startBtn = document.querySelector("#start-btn")
+
 let restartBtn = document.querySelector("#restart-btn")
+
 let splashScreen = document.querySelector("#splash-screen")
+
 let gameoverScreen = document.querySelector("#gameover-screen")
+
 let score = document.querySelector("span")
 
 
 let game;
 
 const startGame = () => {
+    //hide spash screen
     splashScreen.style.display = "none";
-    canvas.style.display = "flex";
+    //show canvas screen
+    canvas.style.display = "flex"
+    //start the game
+
+    //we willhave a class for the Game that when I click
+
     game = new Game();
+
     game.gameLoop();
-};
+
+    }
 
     const restartGame = () => {
         gameoverScreen.style.display = "none";
         canvas.style.display = "flex";
+
         game = new Game();
         game.gameLoop();
-};
+
+}
 
 
 //Events
